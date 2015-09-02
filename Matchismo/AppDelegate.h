a6bd637e579eb40297fc34d15a0f65bc2c8f1f4a
Justin Gazlay <justingazlay@JiMac.local>
2015-09-01 20:28:32 -0600

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Justin Gazlay on 9/1/15.
+//  Copyright (c) 2015 Justin Gazlay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
